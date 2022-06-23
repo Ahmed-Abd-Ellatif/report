d2a319ec656872435f3c597d87a0ec7d28bf0665
Ahmed-Abd-Ellatif <61418344+Ahmed-Abd-Ellatif@users.noreply.github.com>
2022-06-23 16:22:10 +0200

    ###

--- src/app/directive/is-active.directive.ts
+++ src/app/directive/is-active.directive.ts
@@ -1,21 +1,20 @@
-import { Directive, HostListener } from '@angular/core';
+import { Directive, HostListener,ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appIsActive]'
 })
 export class IsActiveDirective {
 
-  constructor() { }
+  constructor(private el : ElementRef) { }
   @HostListener('click')
   imageChange(){
-   
-   var icons = document.querySelectorAll('.icon');
+
+   var icons = document.querySelectorAll('.divRed span'); //icon
 
    icons.forEach(icon =>{
     icon.addEventListener('click',function(){
       icons.forEach(i=> i.classList.remove('active'));
-      icon.classList.add('active');
-      // document.getElementById("pp")?.style.display?'unset';
+      icon.classList.toggle('active');
     });
   });
   
