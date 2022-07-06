218b64ada0fd07d78071222354adcbb46af9bf19
Ahmed-Abd-Ellatif <61418344+Ahmed-Abd-Ellatif@users.noreply.github.com>
2022-07-06 17:17:31 +0200

    6-jul

--- src/app/pages/documents/documents.component.ts
+++ src/app/pages/documents/documents.component.ts
@@ -13,26 +13,42 @@ export class DocumentsComponent implements OnInit {
       title:"Dock Receipt",
       icon:'assets/images/DR.png',
       image:'assets/images/downloadBlue.png',
-      color:'#0078d4'
+      color:'#0078d4',
+      bgc:'#E5F1FB'
     },
     {
-      name:"2018 Mercedes-Benz C63 S",
+      name:"2018 Mercedes-Benz C63 Ss",
       title:"Port Stamp Dock Receipt",
       icon:'assets/images/DRred.png',
       image:'assets/images/downloadBlue.png',
-      color:'#d13438'
+      color:'#d13438',
+      bgc:'#F9E9E7'
     },
     {
-      name:"2018 Mercedes-Benz C63 S",
+      name:"2018 Mercedes-Benz C63 Sss",
       title:"Title File",
       icon:'assets/images/Tgreen.png',
       image:'assets/images/downloadBlue.png',
-      color:'#498205'
+      color:'#498205',
+      bgc:'#e6f3e6'
     },
   ];
+
+
+  showPopup:boolean=false;
+pdf:any
+  openPopup(docs:any){
+    this.showPopup= true
+    this.pdf = docs
+  }
+  closePopup(){
+    this.showPopup= false
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
